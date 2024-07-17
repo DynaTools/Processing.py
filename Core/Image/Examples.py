5dba9b59dbf0caf013d764debbb017dbaff8c92b
Paulo Giavoni <paulo.giavoni@gmail.com>
2024-07-17 11:47:58 +0200

    Update Examples.py

--- Core/Image/Examples.py
+++ Core/Image/Examples.py
@@ -8,21 +8,6 @@ def setup():
     # Set the color of each pixel in the image
     img.loadPixels()  # Load the pixel data of the image
     for i in range(img.width):
-        for j in range(img
-
-
-
-
-def setup():
-    size(800, 800)  # Window size
-    background(255)  # White background
-
-    # createImage() - Creates a new PImage object
-    img = createImage(400, 400, RGB)  # Create a 400x400 RGB image
-
-    # Set the color of each pixel in the image
-    img.loadPixels()  # Load the pixel data of the image
-    for i in range(img.width):
         for j in range(img.height):
             # Set pixel color based on position
             img.pixels[j * img.width + i] = color(i % 255, j % 255, (i + j) % 255)
