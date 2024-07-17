7eba929ffc38d34c6e987b0918848b8e53797016
Paulo Giavoni <paulo.giavoni@gmail.com>
2024-07-17 11:26:27 +0200

    Update 2D Primitives.py

--- Exercises/2D Primitives.py
+++ Exercises/2D Primitives.py
@@ -1,42 +1,45 @@
 def setup():
-    size(800, 800)  # Tamanho da janela
-    background(255)  # Fundo branco
+    size(800, 800)  # Window size
+    background(255)  # White background
 
 def draw():
     # arc
-    fill(255, 0, 0)  # Cor de preenchimento vermelha
-    arc(100, 100, 80, 80, 0, PI + QUARTER_PI)  # Desenha um arco
+    fill(255, 0, 0)  # Red fill color
+    arc(100, 100, 80, 80, 0, PI + QUARTER_PI)  # Draws an arc
     
     # circle
-    fill(0, 255, 0)  # Cor de preenchimento verde
-    circle(250, 100, 80)  # Desenha um círculo
+    fill(0, 255, 0)  # Green fill color
+    circle(250, 100, 80)  # Draws a circle
     
     # ellipse
-    fill(0, 0, 255)  # Cor de preenchimento azul
-    ellipse(400, 100, 80, 120)  # Desenha uma elipse
+    fill(0, 0, 255)  # Blue fill color
+    ellipse(400, 100, 80, 120)  # Draws an ellipse
     
     # line
-    stroke(0)  # Cor da linha preta
-    line(500, 60, 580, 140)  # Desenha uma linha
+    stroke(0)  # Black line color
+    line(500, 60, 580, 140)  # Draws a line
     
     # point
-    stroke(0)  # Cor do ponto preta
-    point(650, 100)  # Desenha um ponto
+    stroke(0)  # Black point color
+    point(650, 100)  # Draws a point
     
     # quad
-    fill(255, 255, 0)  # Cor de preenchimento amarela
-    quad(100, 200, 150, 150, 200, 200, 150, 250)  # Desenha um quadrilátero
+    fill(255, 255, 0)  # Yellow fill color
+    quad(100, 200, 150, 150, 200, 200, 150, 250)  # Draws a quadrilateral
     
     # rect
-    fill(0, 255, 255)  # Cor de preenchimento ciano
-    rect(250, 150, 100, 50)  # Desenha um retângulo
+    fill(0, 255, 255)  # Cyan fill color
+    rect(250, 150, 100, 50)  # Draws a rectangle
     
     # square
-    fill(255, 0, 255)  # Cor de preenchimento magenta
-    square(400, 150, 80)  # Desenha um quadrado
+    fill(255, 0, 255)  # Magenta fill color
+    square(400, 150, 80)  # Draws a square
     
     # triangle
-    fill(128, 128, 128)  # Cor de preenchimento cinza
-    triangle(550, 200, 600, 150, 650, 200)  # Desenha um triângulo
+    fill(128, 128, 128)  # Gray fill color
+    triangle(550, 200, 600, 150, 650, 200)  # Draws a triangle
     
-    noLoop()  # Para o loop draw
+    noLoop()  # Stops the draw loop
+
+run()
+
